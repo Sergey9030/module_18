7c28acb36d7002a47999b092dab65451d37084de
Sergey9030 <709030@mail.ru>
2025-01-08 11:18:46 +0300

    Add files via upload
    Домашнее задание по теме "DTL. Теги, наследование." Исправлена ошибка, связанная с названием страницы.

--- UrbanDjango/task4/views.py
+++ UrbanDjango/task4/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def main_index(request):
-    return render(request, 'fourth_task/main_page1.html')
+    return render(request, 'fourth_task/main_page.html')
 
 def second_index(request):
     context = {'games': ['Atomic Heart', "Cyberpunk 2077"]}
